f11069f092747b2fa53dd078b4351bd546feaae0
Robyn Noble <robynhurd09@gmail.com>
2021-04-06 16:38:20 -0500

    testing functions

--- client.js
+++ client.js
@@ -32,8 +32,32 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+function longTermBonus(employeeNumber) {
+  if (employeeNumber<10000){
+    return 5;
+  }
+  return 0;
+}//end longTermBonus
+
+console.log('should be 5:', longTermBonus(1234));
+console.log('should be 0:', longTermBonus(12345));
+console.log('should be 0:', longTermBonus(10000));
+
+
+
+function employeeIncome(annualSalary) {
+  if (annualSalary > 65000) {
+    return -1;
+  }
+  return 0
+}//end employeeIncome
+
+console.log('should be  0:', employeeIncome(6500));
+console.log('should be  0:', employeeIncome(65000));
+console.log('should be  -1:', employeeIncome(650000));
+
 function employeeRatingBonus(reviewRating) {
-  if (reviewRating< 2){
+  if (reviewRating<= 2){
     return 0;
   }
   else if( reviewRating === 3){
@@ -46,25 +70,34 @@ function employeeRatingBonus(reviewRating) {
     return 10;
   }
 }//end employeeRatingBonus
-function longTermBonus(employeeNumber) {
-  if (employeeNumber<10000){
-    return 5;
-  }
-}//end longTermBonus
-function employeeIncome(annualSalary) {
-  if (annualSalary > 65000) {
-    return -1;
-  }
-  return 0
-}//end employeeIncome
+
+console.log('should be 0:', employeeRatingBonus(1));
+console.log('should be 0:', employeeRatingBonus(2));
+console.log('should be 3:', employeeRatingBonus(3));
+console.log('should be 6:', employeeRatingBonus(4));
+console.log('should be 10:', employeeRatingBonus(5));
+
 function bonusCap(employeeRatingBonus, longTermBonus, employeeIncome) {
   let bonusPer = employeeRatingBonus + longTermBonus + employeeIncome;
   if (bonusPer > 13) {
-    bonusPer = 13
+    bonusPer = 13;
+  }
+  if (bonusPer < 0) {
+    bonusPer = 0;
   }
   return bonusPer;
 }//end bonusCap
 
+console.log('should be 13:', bonusCap(14, 0, 0));
+console.log('should be 13:', bonusCap(13, 0, 0));
+console.log('should be 0:', bonusCap(-1, 0, 0));
+console.log('should be 10:', bonusCap(10, 0, 0));
+
+
+
+function totalBonus(employeeNumber, annualSalary, reviewRating) {
+  let bonusPer
+}
 
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
