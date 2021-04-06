db4474252768ca0643ce82d6afa978dfab29b669
Robyn Noble <robynhurd09@gmail.com>
2021-04-06 16:17:27 -0500

    ind bonus functions begun

--- client.js
+++ client.js
@@ -32,6 +32,40 @@ const employees = [
 ];
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
+function employeeRatingBonus(reviewRating) {
+  if (reviewRating< 2){
+    return 0;
+  }
+  else if( reviewRating === 3){
+    return 4;
+  }
+  else if(reviewRating === 4){
+    return 6;
+  }
+  else if( reviewRating === 5){
+    return 10;
+  }
+}//end employeeRatingBonus
+function longTermBonus(employeeNumber) {
+  if (employeeNumber<10000){
+    return 5;
+  }
+}//end longTermBonus
+function employeeIncome(annualSalary) {
+  if (annualSalary > 65000) {
+    return -1;
+  }
+  return 0
+}//end employeeIncome
+function bonusCap(employeeRatingBonus, longTermBonus, employeeIncome) {
+  let bonusPer = employeeRatingBonus + longTermBonus + employeeIncome;
+  if (bonusPer > 13) {
+    bonusPer = 13
+  }
+  return bonusPer;
+}//end bonusCap
+
+
 
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
